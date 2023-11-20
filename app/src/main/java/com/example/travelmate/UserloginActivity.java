1d79901ad4964d6d40b118f68dd7706dca635efe
Maleesha Saubagya <saubagyamaleesha@gmail.com>
2023-11-20 12:41:36 +0530

    Changed user log in to choose login

--- app/src/main/java/com/example/travelmate/UserloginActivity.java
+++ app/src/main/java/com/example/travelmate/UserloginActivity.java
@@ -9,6 +9,6 @@ public class UserloginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_userlogin);
+        setContentView(R.layout.activity_choose_log_in);
     }
 }
