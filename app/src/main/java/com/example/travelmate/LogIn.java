5985518f434fe2cc6090eff3c90ada28f0dd3dae
Maleesha Saubagya <saubagyamaleesha@gmail.com>
2023-11-22 07:50:35 +0530

    Backend
    Register page and Guide register page back end coded successful. now we can move registers data to fire base

--- app/src/main/java/com/example/travelmate/LogIn.java
+++ app/src/main/java/com/example/travelmate/LogIn.java
@@ -2,13 +2,36 @@ package com.example.travelmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
-
+    TextView SignUp;
+    Button GoHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        SignUp=findViewById(R.id.SignUp);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this,Sign_up.class);
+                startActivity(intent);
+            }
+        });
+
+        GoHome=findViewById(R.id.button5);
+        GoHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
