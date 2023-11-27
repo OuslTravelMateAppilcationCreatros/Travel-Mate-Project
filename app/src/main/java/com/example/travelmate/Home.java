1e1ef685df016145505d32658a6ba155df9fc43d
Maleesha Saubagya <saubagyamaleesha@gmail.com>
2023-11-27 23:05:29 +0530

    Home page update
    update home page

--- app/src/main/java/com/example/travelmate/Home.java
+++ app/src/main/java/com/example/travelmate/Home.java
@@ -1,5 +1,6 @@
 package com.example.travelmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,16 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
     ImageView Go;
-    TextView PF;
+    TextView PF, Name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,9 @@ public class Home extends AppCompatActivity {
 
         Go=findViewById(R.id.Go);
         PF =findViewById(R.id.textVie1);
+        Name =findViewById(R.id.Name);
+
+
         Go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +44,38 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+
+    public void showUserData(){
+
+        Intent intent = getIntent();
+
+        String nameUser = intent.getStringExtra("name");
+        Name.setText(nameUser);
+
+    }
+    public void passUserData(){
+        String userUsername = Name.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    String nameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 
 
 }
