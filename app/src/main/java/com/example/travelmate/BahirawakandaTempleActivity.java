8375c4d09f3c5555f138ea4042e0c303f1aae523
dskdissanayake99 <150094371+dskdissanayake99@users.noreply.github.com>
2023-11-28 22:09:36 +0530

    uk
    change line

---
+++ app/src/main/java/com/example/travelmate/BahirawakandaTempleActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BahirawakandaTempleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bahirawakanda_temple);
+    }
+}
