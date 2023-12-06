76b1b9d2ba298937fff99185414d54be4e0c4c12
dskdissanayake99 <150094371+dskdissanayake99@users.noreply.github.com>
2023-12-06 20:58:03 +0530

    eastern
    create google map

--- app/src/main/java/com/example/travelmate/EasternProvince.java
+++ app/src/main/java/com/example/travelmate/EasternProvince.java
@@ -9,13 +9,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class EasternProvince extends AppCompatActivity {
+<<<<<<< Updated upstream
     Button BTN4;
+=======
+    Button BTN6;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eastern_province);
 
+<<<<<<< Updated upstream
         BTN4 = findViewById(R.id.btn5);
 
 
@@ -26,6 +31,20 @@ public class EasternProvince extends AppCompatActivity {
             }
         });
     }
+=======
+
+       BTN6 = findViewById(R.id.btn6);
+
+
+        BTN6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.google.com/maps/dir//Trincomalee/@8.5874465,81.1739265,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3afbbcb6902dbe27:0x7de76a7a331b0fbb!2m2!1d81.2152121!2d8.5873638!3e0?authuser=0&entry=ttu");
+            }
+        });
+    }
+
+>>>>>>> Stashed changes
     private void gotoUrl(String s) {
         Uri uri = Uri.parse(s);
         startActivity(new Intent(Intent.ACTION_VIEW,uri));
