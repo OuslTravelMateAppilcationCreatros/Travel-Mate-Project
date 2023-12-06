b9514586451dd235ca8022159e98afb1ca2a7c23
dskdissanayake99 <150094371+dskdissanayake99@users.noreply.github.com>
2023-12-06 12:52:58 +0530

    north
    update page

--- app/src/main/java/com/example/travelmate/NorthwestProvince.java
+++ app/src/main/java/com/example/travelmate/NorthwestProvince.java
@@ -22,7 +22,8 @@ public class NorthwestProvince extends AppCompatActivity {
         BTN2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gotoUrl("https://www.google.com/maps/dir//Yapahuwa/@7.8293673,80.260069,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3afccf94f3a356bf:0x9d90904281c78146!2m2!1d80.3013547!2d7.8292843!3e0?authuser=0&entry=ttu"}
+                gotoUrl("https://www.google.com/maps/dir//Yapahuwa/@7.8293673,80.260069,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3afccf94f3a356bf:0x9d90904281c78146!2m2!1d80.3013547!2d7.8292843!3e0?authuser=0&entry=ttu");
+            }
         });
     }
     private void gotoUrl(String s) {
